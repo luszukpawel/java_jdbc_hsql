37f370ca7f73937987527905f8dd38f2a9bd2240
Paweł Luszuk <luszukpawel@gmail.com>
2016-11-21 22:15:06 +0100

    basic classes fix + getters/setters + constructors

--- src/main/java/com/project/crud/domain/Ammunition.java
+++ src/main/java/com/project/crud/domain/Ammunition.java
@@ -1,58 +1,82 @@
 package main.java.com.project.crud.domain;
 
-public class Ammunition {
+public class Ammunition
+{
 	private int id;
-	
+
 	private String name;
+	private float caliber;
 	private int cost;
-	private int toxicity;
 	private int Weapon_id;
-	
-	public Ammunition(String name, int cost, int toxicity) {
-		super();
-		this.name = name;
-		this.cost = cost;
-		this.toxicity = toxicity;
+
+	public Ammunition()
+	{
 	}
 
-	public Ammunition(String name, int cost, int toxicity, int Weapon_id) {
+	Ammunition(String name, int cost, float caliber)
+	{
 		super();
 		this.name = name;
 		this.cost = cost;
-		this.toxicity = toxicity;
+		this.caliber = caliber;
+	}
+
+	public Ammunition(String name, int cost, float caliber, int Weapon_id)
+	{
+		super();
+		this.name = name;
+		this.cost = cost;
+		this.caliber = caliber;
 		this.Weapon_id = Weapon_id;
 	}
-	
-	public Ammunition(){}
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
+
+	public void setId(int id)
+	{
 		this.id = id;
 	}
-	public int getWeapon_id() {
+
+	public int getId()
+	{
+		return id;
+	}
+
+	public int getWeapon_id()
+	{
 		return Weapon_id;
 	}
-	public void setWeapon_id(int Weapon_id) {
+
+	public void setWeapon_id(int Weapon_id)
+	{
 		this.Weapon_id = Weapon_id;
 	}
-	public String getName() {
+
+	public String getName()
+	{
 		return name;
 	}
-	public void setName(String name) {
+
+	public void setName(String name)
+	{
 		this.name = name;
 	}
-	public int getCost() {
+
+	public int getCost()
+	{
 		return cost;
 	}
-	public void setCost(int cost) {
+
+	public void setCost(int cost)
+	{
 		this.cost = cost;
 	}
-	public int getToxicity() {
-		return toxicity;
+
+	public float getcaliber()
+	{
+		return caliber;
 	}
-	public void setToxicity(int toxicity) {
-		this.toxicity = toxicity;
+
+	public void setcaliber(float caliber)
+	{
+		this.caliber = caliber;
 	}
 }
--- src/main/java/com/project/crud/domain/Weapon.java
+++ src/main/java/com/project/crud/domain/Weapon.java
@@ -1,32 +1,69 @@
 package main.java.com.project.crud.domain;
 
-public class Weapon {
+public class Weapon
+{
 	private int id;
-	
+	private float cost;
+	private String model;
+	private String producer;
 
-	public int getId() {
+	public int getId()
+	{
 		return id;
 	}
 
-	public void setId(int id) {
+	public void setId(int id)
+	{
 		this.id = id;
 	}
 
-	private String model;
+	public float getCost()
+	{
+		return cost;
+	}
 
-	public String getModel() {
+	public void setCost(float cost)
+	{
+		this.cost = cost;
+
+	}
+
+	public String getProducer()
+	{
+		return producer;
+	}
+
+	public void setProducer(String producer)
+	{
+		this.producer = producer;
+	}
+
+	public String getModel()
+	{
 		return model;
 	}
 
-	public void setName(String model) {
+	public void setModel(String model)
+	{
 		this.model = model;
-	}	
-	
-	public Weapon(String _model) {
+	}
+
+	public Weapon(int id, float cost, String model, String producer)
+	{
+		super();
+		this.id = id;
+		this.cost = cost;
+		this.model = model;
+		this.producer = producer;
+	}
+
+	public Weapon(String _model)
+	{
 		this.model = _model;
 	}
-	
-	public Weapon(){
-	
+
+	public Weapon()
+	{
+
 	}
 }
